f2c613ed161c4a3757b4c798bed77d36b376dd1c
Lee-Junhee <jlee02@stuy.edu>
2019-05-07 00:07:31 -0400

    did the homework

---
+++ InsertionSorter.java
@@ -0,0 +1,45 @@
+/**
+  Implement an insertion sort, in the Sorters structure
+ */
+import java.util.ArrayList;
+
+public class InsertionSorter extends Sorter {
+
+    /**
+      Construct an instance to process the user's data
+     */
+    public InsertionSorter(  ArrayList< String> usersData) {
+      super(usersData);
+      mySort();  
+      // your code here, if necessary
+    }
+    
+
+    /**
+      sort the user's data, implementing insertion sort
+      insertionSort based off work from Piazza@404, work by Si Liang Lei
+      */
+    public void mySort() {
+      for(int elementToInsert = 1;
+        elementToInsert < elements.size();
+        insert(elementToInsert++)){}
+    }
+
+    private void insert(int numSorted) {
+      String valueToInsert = elements.get(numSorted);
+      int indexToCompare;
+      for(indexToCompare = numSorted - 1;
+          indexToCompare > -1;
+          indexToCompare--) {
+            if(valueToInsert.compareTo(elements.get(indexToCompare)) < 0) {
+              elements.set(indexToCompare + 1, elements.get(indexToCompare));
+            }else {
+              elements.set(indexToCompare + 1, valueToInsert);
+              break;
+            }
+          }
+      if (indexToCompare < 0) {
+        elements.set(0, valueToInsert);
+      }
+    }
+}
---
+++ SelectionSorter.java
@@ -0,0 +1,40 @@
+/**
+  Implement an insertion sort, in the Sorters structure
+ */
+import java.util.ArrayList;
+
+public class SelectionSorter extends Sorter {
+
+    /**
+      Construct an instance to process the user's data
+     */
+    public SelectionSorter(  ArrayList< String> usersData) {
+      super(usersData);
+      mySort();  
+      // your code here, if necessary
+    }
+    
+
+    /**
+      sort the user's data, implementing insertion sort
+      insertionSort based off work from Piazza@404, work by Si Liang Lei
+      */
+    public void mySort() {
+        for(int numSorted = 0;
+            numSorted < elements.size() - 1;
+            select(numSorted++)){}
+    }
+
+    public void select(int startAt) {
+        int dweebIndex;
+        for(int indexToCompare = (dweebIndex = startAt) + 1;
+            indexToCompare < elements.size();
+            indexToCompare++){
+                if(elements.get(dweebIndex).compareTo(elements.get(indexToCompare)) > 0) {
+                    dweebIndex = indexToCompare;
+                }
+            }
+        elements.set(startAt, elements.set(dweebIndex, elements.get(startAt)));
+    }
+
+}
